940e6bb23c5966db9cc0094f7773920500d03bf0
Nathan Pickard <nathan.pickard87@gmail.com>
2020-12-29 17:40:47 -0800

    Added models for city, current conditions, hourly/weekly forecast, and location/weather data

---
+++ src/app/models/current-conditions/current-conditions.ts
@@ -0,0 +1,14 @@
+export class CurrentConditions {
+  longitude = '';
+  latitude = '';
+  city = '';
+  state = '';
+  description = '';
+  temp = '';
+  humitity = '';
+  windSpeed = '';
+  windDirection = '';
+  sunrise = '';
+  sunset = '';
+  icon = '';
+}
---
+++ src/app/models/weather-data/weather-data.ts
@@ -0,0 +1,11 @@
+export class WeatherData {
+  forecastURL = '';
+  forecast: {};
+  currentConditions = '';
+  weeklyForecast = '';
+  hourlyForecast = '';
+  NoaaWeeklyForecastUrl = '';
+  NoaaHourlyForecastUrl = '';
+  errorMessage = '';
+  weatherDate: Date = new Date();
+}
