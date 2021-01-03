18e1d35e8ca56cc577c514f8fac98338d7834bb0
Nathan Pickard <nathan.pickard87@gmail.com>
2021-01-02 17:02:54 -0800

    Added weather component properties

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { Breakpoints, BreakpointObserver } from '@angular/cdk';
 
 import { WeatherData } from '../models/weather-data/weather-data';
+import { LocationData } from '../models/location-data/location-data';
+import { City } from '../models/city/city';
+
+import { WeatherService } from '../weather.service';
 
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-weather',
@@ -11,7 +18,31 @@ import { Observable } from 'rxjs';
 })
 export class WeatherComponent implements OnInit {
 
-  constructor() { }
+  weatherData: WeatherData;
+  lat: string;
+  long: string;
+  cardsDesktop = [];
+  cardsMobile = [];
+  displayValues = false;
+  spinnerSize = 8;
+  locationData: LocationData = new LocationData();
+  citiesCtrl = new FormControl();
+  filteredCities: Observable<City[]>;
+  cities = [];
+  selectedLocations = '';
+
+  cards = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(({ matches }) => {
+        if (matches) {
+          return this.cardsMobile;
+        } else {
+          return this.cardsDesktop;
+        }
+      })
+    );
+
+  constructor(private breakpointObserver: BreakpointObserver, public weatherService: WeatherService) { }
 
   ngOnInit(): void {
   }
