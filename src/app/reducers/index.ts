a6e7716d3f17097ffca502cf6e7f8e724663a817
Nathan Pickard <nathan.pickard87@gmail.com>
2021-01-12 18:58:55 -0800

    Location actions and initial weather state

--- src/app/reducers/index.ts
+++ src/app/reducers/index.ts
@@ -9,15 +9,40 @@ import {
 import { environment } from '../../environments/environment';
 import { WeatherData } from '../models/weather-data/weather-data';
 import { WeatherActionTypes, WeatherAction } from '../actions/weather.actions'
+import { LocationActionTypes, LocationAction } from '../actions/location.actions'
+import { LocationData } from '../models/location-data/location-data';
 
-
-export interface State {
-
+export interface WeatherState {
+  weatherData: WeatherData | null;
 }
 
-export const reducers: ActionReducerMap<State> = {
-
+const initialWeatherState: WeatherState = {
+  weatherData: null
 };
 
+export interface LocationState {
+  location: LocationData | null;
+  error: string | null;
+}
 
-export const metaReducers: MetaReducer<State>[] = !environment.production ? [] : [];
+const initialLocationState: LocationState = {
+  location: null,
+  error: null
+};
+
+export interface AppState {
+  weather: WeatherState;
+  location: LocationState;
+}
+
+export function weatherReducer(state: WeatherState = initialWeatherState, action: WeatherAction): WeatherState {
+  switch (action.type) {
+    case WeatherActionTypes.LoadWeather:
+      return {
+        weatherData: action.payload.weatherData
+      };
+
+    default:
+      return state;
+  }
+}
