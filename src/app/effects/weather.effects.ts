a1c403b32e1989269fee497312bac7af800e2e7b
Nathan Pickard <nathan.pickard87@gmail.com>
2021-01-15 20:25:21 -0800

    loadLocation effect in weather effects

--- src/app/effects/weather.effects.ts
+++ src/app/effects/weather.effects.ts
@@ -1,13 +1,34 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, Effect, ofType } from '@ngrx/effects';
+import { Store } from '@ngrx/store'
+
+import { LoadWeather } from '../actions/weather.actions';
+import { LocationActionTypes, LocationsError, LoadLocations } from '../actions/location.actions';
+
+import { AppState } from '../reducers';
+
+import { WeatherService } from '../weather.service';
+
+import { map, mergeMap, catchError } from 'rxjs/operators';
+import { of } from 'rxjs';
 
 
 
 @Injectable()
 export class WeatherEffects {
 
+  constructor(private actions$: Actions, private store: Store<AppState>, private weatherService: WeatherService) { }
 
-
-  constructor(private actions$: Actions) {}
-
+  @Effect()
+  loadLocation$ = this.actions$
+    .pipe(
+      ofType<LoadLocations>(LocationActionTypes.LoadLocations),
+      mergeMap((action) => this.weatherService.getWeather(action.payload.locationData)
+        .pipe(
+          map(weather => {
+            return (new LoadWeather({ weatherData: weather }));
+          }),
+          catchError((errorMessage) => of(new LocationsError({ error: errorMessage })))
+        ))
+    );
 }
