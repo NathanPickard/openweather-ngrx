6d4df3d41b54e9d5db400e36dc51dc9a0b566ca3
Nathan Pickard <nathan.pickard87@gmail.com>
2021-01-30 20:20:15 -0800

    Added store to current conditions and hourly forecast components

--- src/app/cards/hourly-forecast/hourly-forecast.component.ts
+++ src/app/cards/hourly-forecast/hourly-forecast.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { WeatherData } from 'src/app/models/weather-data/weather-data';
+import { Store, select } from '@ngrx/store';
+import { AppState, selectWeather } from '../../reducers';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-hourly-forecast',
@@ -8,17 +11,21 @@ import { WeatherData } from 'src/app/models/weather-data/weather-data';
 })
 export class HourlyForecastComponent implements OnInit {
 
-  data: WeatherData | undefined;
+  // @Input()
+  // set weatherData(weatherData: WeatherData) {
+  //   this.data = weatherData || null;
+  // }
+
+  // data: WeatherData | undefined;
+
   displayedColumns: string[] = ['Time', 'Temp', 'Wind', 'Condition'];
 
-  @Input()
-  set weatherData(weatherData: WeatherData) {
-    this.data = weatherData || null;
-  }
-
-  constructor() { }
+  data$: Observable<WeatherData> | undefined;
+  
+  constructor(private store: Store<AppState>) { }
 
   ngOnInit(): void {
+    this.data$ = this.store.pipe(select(selectWeather));
   }
 
 }
